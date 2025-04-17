575cf3466d17658cf9c948c2adf2430c57953516
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2025-04-17 15:36:23 +0300

    Create hook to get responsibilityNode

--- dashboard/src/hooks/useResponsibility.ts
+++ dashboard/src/hooks/useResponsibility.ts
@@ -3,13 +3,22 @@ import useApi from "./useApi";
 
 import { useCallback, useEffect, useState } from "react";
 
-function useResponsibility() {
+function useResponsibility(id?: string) {
   const [responsibility, setResponsibility] = useState([]);
+  const [responsibilityNodes, setResponsibilityNodes] = useState({
+    id: "",
+    label: "",
+    icon: "",
+    Nodes: [{}],
+  });
 
   const { loading, error, handleResponse } = useApi();
 
   useEffect(() => {
     getResponsibility();
+    if (id) {
+      getResponsibilityWithNode(id);
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -26,10 +35,24 @@ function useResponsibility() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const getResponsibilityWithNode = useCallback((id) => {
+    handleResponse(
+      http.get(`/responsibilities/${id}`),
+      (response) => {
+        setResponsibilityNodes(response.data);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return {
     loading,
     error,
     responsibility,
+    responsibilityNodes,
   };
 }
 
