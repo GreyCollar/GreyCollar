19e15674092dfad4096fd7dc36bff4b5f6e11aca
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2025-04-28 20:30:54 +0300

    Remove env and ImportMeta

--- dashboard/src/vite-env.d.ts
+++ dashboard/src/vite-env.d.ts
@@ -1,14 +1,3 @@
-interface ImportMetaEnv {
-  readonly VITE_CLIENT_ID: string;
-  readonly VITE_OAUTH_REDIRECT_URI: string;
-  readonly VITE_CLIENT_SECRET: string;
-  // more env variables...
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
-
 interface CustomWindow extends Window {
   google?: array;
 }
