8b670ba69dffd46f249e26a771994df0f745cb92
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2025-05-03 12:00:01 +0300

    Remove integrations function and use integration

--- api/src/functions/integration.ts
+++ api/src/functions/integration.ts
@@ -5,8 +5,10 @@ import {
 
 import Colleague from "../models/Colleague";
 import Integration from "../models/Integration";
+import Integrations from "../integrations/integrations";
+import axios from "axios";
 
-async function list({
+async function show({
   colleagueId,
   teamId,
 }: {
@@ -43,6 +45,59 @@ async function list({
     }));
 }
 
+async function create({
+  code,
+  clientId,
+  clientSecret,
+  redirectUri,
+  tokenUrl,
+}: {
+  code: string;
+  clientId: string;
+  clientSecret: string;
+  redirectUri: string;
+  tokenUrl: string;
+}) {
+  const params = new URLSearchParams();
+  params.append("code", code);
+  params.append("client_id", clientId);
+  params.append("client_secret", clientSecret);
+  params.append("redirect_uri", redirectUri);
+  params.append("grant_type", "authorization_code");
+
+  const response = await axios.post(tokenUrl, params, {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+
+  const tokens = response.data;
+
+  return tokens;
+}
+
+async function list() {
+  if (!Integrations || Integrations.length === 0) {
+    throw new NotFoundError();
+  }
+
+  return Integrations.map((provider) => ({
+    id: provider.id,
+    provider: provider.provider,
+    description: provider.description,
+    action: provider.action,
+    direction: provider.direction,
+    oauth: provider.oauth
+      ? {
+          scope: provider.oauth.scope,
+          tokenUrl: provider.oauth.tokenUrl,
+          clientId: provider.oauth.clientId,
+          redirectUri: provider.oauth.redirectUri,
+        }
+      : null,
+  }));
+}
+
 async function get({ integrationId }: { integrationId: string }) {
   const integrationItem = await Integration.findByPk(integrationId);
 
@@ -53,4 +108,4 @@ async function get({ integrationId }: { integrationId: string }) {
   return integrationItem.toJSON();
 }
 
-export default { list, get };
+export default { show, get, create, list };
