1d74e83e4bbd35380a07a41fdeee9e3ac3cd5532
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2025-05-06 11:36:28 +0300

    Update the AuthHandler with new structure

--- dashboard/src/components/LoginHandler/AuthHandler.tsx
+++ dashboard/src/components/LoginHandler/AuthHandler.tsx
@@ -3,27 +3,33 @@ import { useEffect } from "react";
 const AuthHandler = ({ skill, getTokens, onAuthSuccess }) => {
   useEffect(() => {
     const script = document.createElement("script");
-    script.src = "https://accounts.google.com/gsi/client";
+    script.src = skill?.oauth.clientScript;
     script.async = true;
     document.body.appendChild(script);
   }, []);
 
   const handleLogin = (id, isTeam) => {
-    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${encodeURIComponent(
-      skill.oauth.clientId
-    )}&redirect_uri=${encodeURIComponent(
-      skill.oauth.redirectUri
-    )}&response_type=code&access_type=offline&prompt=consent&include_granted_scopes=false&scope=${encodeURIComponent(
-      skill.oauth.scope
-    )}`;
+    const { authUrl, clientId, redirectUri, scope } = skill.oauth;
+
+    const queryParams = new URLSearchParams({
+      client_id: clientId,
+      redirect_uri: redirectUri,
+      response_type: "code",
+      access_type: "offline",
+      prompt: "consent",
+      include_granted_scopes: "false",
+      scope: scope,
+    });
+
+    const authUrlWithParams = `${authUrl}?${queryParams.toString()}`;
 
     const width = 500;
     const height = 600;
     const left = window.screenX + (window.innerWidth - width) / 2;
     const top = window.screenY + (window.innerHeight - height) / 2;
     const popup = window.open(
-      authUrl,
-      "google_oauth_popup",
+      authUrlWithParams,
+      "oauth_popup",
       `width=${width},height=${height},left=${left},top=${top}`
     );
 
