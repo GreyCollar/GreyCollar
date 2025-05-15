1d2256859fcecd67858a95bcb30af284d2af3570
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2025-05-15 16:45:27 +0300

    Remove generateNode

--- api/src/lib/llm.ts
+++ api/src/lib/llm.ts
@@ -62,9 +62,6 @@ async function generate({
     messages.unshift(policy);
   }
 
-  console.log("messages", messages);
-  console.log("llm", llm);
-
   return await llm.generate({
     model,
     messages,
@@ -73,76 +70,4 @@ async function generate({
   });
 }
 
-async function generateNode({
-  dataset,
-  content,
-  json_format,
-  flow,
-}: {
-  dataset?: { role: string; content: string };
-  json_format: string;
-  content: { role: string; content: string; text?: string }[];
-  flow: [];
-}) {
-  const messages: {
-    role: string;
-    content?: string;
-    text?: string;
-    flow?: [];
-  }[] = [];
-
-  console.log(content);
-  console.log(flow);
-
-  if (dataset) {
-    messages.push({ role: dataset.role, content: dataset.content });
-  }
-
-  if (content.length > 1) {
-    content.forEach((message) => {
-      messages.push({
-        role: message.role === "ai" ? "assistant" : message.role,
-        content: message?.content || message?.text,
-      });
-    });
-  }
-
-  if (flow) {
-    messages.push({
-      role: "system",
-      content: JSON.stringify(flow),
-    });
-  }
-
-  messages.push({
-    role: "user",
-    content: typeof content === "object" ? JSON.stringify(content) : content,
-  });
-
-  messages.push({
-    role: "system",
-    content: `json_format: ${json_format}`,
-  });
-
-  const response = await llm.generate({ messages });
-
-  const text = Array.isArray(response.choices)
-    ? response.choices[0].message.content
-    : typeof response === "string"
-    ? response
-    : JSON.stringify(response);
-
-  try {
-    const parsed = JSON.parse(text);
-    console.log("flow", parsed?.flow);
-    console.log("response", parsed?.response);
-
-    return parsed;
-  } catch (err) {
-    console.error("Failed to parse JSON:\n", text);
-    throw err;
-  }
-}
-
-export { generate, generateNode };
-
+export { generate };
