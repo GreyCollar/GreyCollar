743cb9a9a87373b4142507b23a5ffd4666666bf4
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2025-05-29 14:53:05 +0300

    Add button to switch between charts

--- dashboard/src/pages/organizations.tsx
+++ dashboard/src/pages/organizations.tsx
@@ -1,10 +1,32 @@
-import OrganizationalChart from "../widgets/OrganizationChart/OrganizationalChart";
-import React from "react";
+import { IconButton } from "@mui/material";
+import { Iconify } from "@nucleoidai/platform/minimal/components";
+import OrganizationChart from "../widgets/OrganizationChart/OrganizationChart";
+import TeamChart from "../widgets/TeamChart/TeamChart";
+
+import { Box, Button } from "@mui/material";
+import React, { useState } from "react";
 
 const Organizations = () => {
+  const [isOldChartOpen, setIsOldChartOpen] = useState(false);
+
   return (
     <>
-      <OrganizationalChart sx={{ margin: 0.2, lineHeight: "15px" }} />
+      <Box sx={{ position: "relative", width: "100%", height: "100%" }}>
+        <Box sx={{ position: "absolute", top: 16, right: 100, zIndex: 10 }}>
+          <IconButton
+            data-cy="organization-button"
+            sx={{
+              bgcolor: (theme) => theme.palette.primary.main,
+              color: (theme) => theme.palette.text.primary,
+            }}
+            onClick={() => setIsOldChartOpen(!isOldChartOpen)}
+          >
+            <Iconify icon={"ri:node-tree"} width={24} />
+          </IconButton>
+        </Box>
+        <TeamChart sx={{ margin: 0.2, lineHeight: "15px" }} />
+      </Box>
+      <OrganizationChart open={isOldChartOpen} setOpen={setIsOldChartOpen} />
     </>
   );
 };
