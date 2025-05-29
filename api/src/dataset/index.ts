f35f71ac3bc429f1ea6741b33277de7a202f28cb
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2025-05-29 22:59:12 +0300

    Add responsibility name dataset

--- api/src/dataset/index.ts
+++ api/src/dataset/index.ts
@@ -1,6 +1,7 @@
 import chat from "./train/chat.json";
 import policy from "./policy.json";
 import responsibility from "./train/responsibility.json";
+import responsibilityName from "./train/responsibility-name.json";
 import task from "./train/task.json";
 import teamChat from "./train/team-chat.json";
 
@@ -32,6 +33,12 @@ export default {
       role: "system" as const,
       content: JSON.stringify({
         train: responsibility,
+      }),
+    },
+    responsibilityName: {
+      role: "system" as const,
+      content: JSON.stringify({
+        train: responsibilityName,
       }),
     },
   },
