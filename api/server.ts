7d1482d469766327eb68874d81f1dbac6b104ab8
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2025-06-04 12:10:25 +0300

    Remove socket from api

--- api/server.ts
+++ api/server.ts
@@ -5,7 +5,6 @@ import config from "./config";
 import dotenv from "dotenv";
 import http from "http";
 import models from "./src/models";
-import sessions from "./src/socket/sessions";
 
 dotenv.config();
 
@@ -21,16 +20,11 @@ platform.init(config).then(() => {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const app = require("./src/app").default;
   const server = http.createServer(app);
-  const io = new Server(server, {
-    cors: {
-      origin: "*",
-    },
-  });
 
   models.init();
-  sessions.setup(io);
 
   server.listen(process.env.PORT || 4000, () => {
     console.log(`Server is running on port ${process.env.PORT || 4000}`);
   });
 });
+
