bbb48d69efd90dfa452d0fb8d19473396862ac1e
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2025-09-09 14:10:51 +0300

    Remove unused import from app.ts
    Deleted the unused 'subscribe' import from '@nucleoidai/node-event' to clean up the code.

--- api/src/app.ts
+++ api/src/app.ts
@@ -16,7 +16,6 @@ import projects from "./routes/projects";
 import responsibilities from "./routes/responsibilities";
 import sessions from "./routes/sessions";
 import statistics from "./routes/statistics";
-import { subscribe } from "@nucleoidai/node-event";
 import supervisings from "./routes/supervisings";
 import tasks from "./routes/tasks";
 import teamDetails from "./routes/teamDetails";
