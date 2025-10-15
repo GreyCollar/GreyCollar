5aad515aaa61eee1813451bf7f64723d3c56a778
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2025-10-15 12:33:28 +0300

    Update Oracle config paths and node-event-test-package version
    Changed instantClientPath and added walletPath in both api and communication config.txq.ts files to use absolute Windows paths. Updated node-event-test-package dependency to version 1.1.42 in both api and communication package.json files.

--- communication/config.txq.ts
+++ communication/config.txq.ts
@@ -4,7 +4,8 @@ const txqConfig = {
     "(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.us-ashburn-1.oraclecloud.com))(connect_data=(service_name=g0a7523bd2f0a8f_gas3p3tsba5e6jch_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))",
   user: "txeventq_user",
   password: "FBKBwFb1dSrH",
-  instantClientPath: "../driver/instantclient_23_9",
+  instantClientPath: "C:\\Users\\Halil\\Desktop\\nucleoid\\greycollar\\oracle\\driver\\instantclient_23_9",
+  walletPath: "C:\\Users\\Halil\\Desktop\\nucleoid\\greycollar\\oracle\\wallet",
 };
 
 export default txqConfig;
